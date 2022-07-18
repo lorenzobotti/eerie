bee6486183113ba407b4afc5db3e16a68823435e
Lorenzo Botti <bottilorenzobg@gmail.com>
2022-07-18 18:40:46 +0200

    Rename markdown file in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use eerie::Files;
 
 fn main() {
-    let test = include_str!("../tests/calculator.eer");
+    let test = include_str!("../tests/calculator.eer.md");
     let (files, _) = Files::from_str(test).unwrap();
 
     dbg!(files.0);
